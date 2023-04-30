edf2734d7eb15208efe7b85035ace5c3442f2d96
savio <savioanjos2020@gmail.com>
2023-04-30 15:28:17 -0300

    Finalizando lógica para buscar valor na api

--- App.js
+++ App.js
@@ -44,6 +44,17 @@ async function getListCoins(url) {
   return data;
 }
 
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fectch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinsList = Object.keys(selectListQuotationsG).map((key) => {
+    selectListQuotationsG[key];
+  });
+  let dataG = queryCoinsList.reverse();
+  return dataG;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
