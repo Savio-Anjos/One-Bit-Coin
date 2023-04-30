f1c44c0fedbcc108d02bffdfdbe611ef45178f80
savio <savioanjos2020@gmail.com>
2023-04-30 13:42:19 -0300

    Montando url para consultar api

--- App.js
+++ App.js
@@ -5,6 +5,31 @@ import HistoryGraphic from "./src/components/HistoryGraphic";
 import QuotationList from "./src/components/QuotationsList";
 import QuotationItems from "./src/components/QuotationsList/QuotationItems";
 
+function addZero(number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays;
+
+  const end_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+
+  date.setDate(date.getDate() - listLastDays);
+
+  const start_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
