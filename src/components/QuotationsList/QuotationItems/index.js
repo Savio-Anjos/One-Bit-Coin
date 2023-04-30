ce75e555615309913ceffdd79f99a0e4121f2889
savio <savioanjos2020@gmail.com>
2023-04-30 00:46:46 -0300

    Estilizando QuotationItems

--- src/components/QuotationsList/QuotationItems/index.js
+++ src/components/QuotationsList/QuotationItems/index.js
@@ -1,3 +1,17 @@
+9;
+10;
+11;
+12;
+13;
+14;
+15;
+16;
+17;
+18;
+19;
+20;
+21;
+22;
 import React from "react";
 import { View, Text, Image } from "react-native";
 import styles from "./styles";
@@ -8,14 +22,14 @@ export default function QuotationItems() {
       <View style={styles.contextLeft}>
         <View style={styles.boxLogo}>
           <Image
-            style={styles.logoBitcoin}
+            style={styles.logBitcoin}
             source={require("../../../img/logobitcoin.png")}
           />
-          <Text style={styles.dayCotation}>29/04/2023</Text>
+          <Text style={styles.dayCotation}>07/05/2021</Text>
         </View>
-        <View style={styles.contextRight}>
-          <Text style={styles.price}>$ 53331.052</Text>
-        </View>
+      </View>
+      <View style={styles.contextRigth}>
+        <Text style={styles.price}>$ 53331.052</Text>
       </View>
     </View>
   );
